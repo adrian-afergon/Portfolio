fdf2a1eb60591f263337f8e1d6f6d292a2f0472b
Adrián Ferrera González <adrian.afergon@gmail.com>
2014-06-28 21:32:56 +0100

    Update 2
    Add responsive, but need update de height

--- js/header-size.js
+++ js/header-size.js
@@ -1,26 +1,59 @@
 var SMALL ='40px';
 var BIG = '100px';
+var MINWITH = 480;
 var ANIMATION_TIME = 600;
 
-$(document).ready(function(){
+var SMALL_RESPONSIVE ='60px';
 
+/**
+	Es necesario ajustar el tamaño del header en el css de tal forma que en el tamaño para tablet
+	se ajuste el menú (o, cambias el tamaño para que trate la tablet tambien como el movil y no lo
+	rediimensione)
+
+
+	Controlar que cuando estás en movil no es necesario cambiar el tamaño, sino dejar el que tiene por
+	defeto de 60px
+*/
+
+
+function resizeHeader(){
+	if($(document).scrollTop() > 0){
+		if($('header').data('size') == 'big'){
+			$('header').data('size','small');
+			$('header').stop().animate({
+				height:SMALL
+			},ANIMATION_TIME);
+		}
+	}
+	else{
+		if($('header').data('size') == 'small'){
+			$('header').data('size','big');
+			$('header').stop().animate({
+				height:BIG
+			},ANIMATION_TIME);
+		}  
+	}
+}
+
+$(document).ready(function(){
 	$('header').data('size','big');
 	$(window).scroll(function(){
-	    if($(document).scrollTop() > 0){
-			if($('header').data('size') == 'big'){
-				$('header').data('size','small');
-				$('header').stop().animate({
-				    height:SMALL
-				},ANIMATION_TIME);
-			}
+		if ($(window).width() >= MINWITH){
+			resizeHeader();
 		}
-		else{
-		    if($('header').data('size') == 'small'){
-				$('header').data('size','big');
-				$('header').stop().animate({
-				    height:BIG
-				},ANIMATION_TIME);
-			}  
+	});
+	$(window).on('resize', function(){
+      	if ($(window).width() >= MINWITH){
+			resizeHeader();
 		}
+      	else{
+      		//
+      			//No me gusta, posible refactorización
+      		//
+      		$('header').data('size','big');
+					$('header').stop().animate({
+					    height:BIG
+					},ANIMATION_TIME);
+      	}
 	});
 });
